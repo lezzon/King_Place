9588eb5c14a69841b6bc4a81a0d2d0318951bdfc
Lezzon <voddol69@gmail.com>
2020-06-16 12:41:46 +0300

    Changing King's place script

--- King_Place.py
+++ King_Place.py
@@ -2,6 +2,6 @@ x = ["a","b","c","d","e","f","g","h"]
 print(type(x))      
 for i in x:
     if i == "e":
-        print("King`s place")
+        print("This is the King`s place")
         continue
     print(i)    
